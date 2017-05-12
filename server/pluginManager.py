0622da64d02a095a8eaa2e312b706b10176a726a
Francesco <ridolfinotv@gmail.com>
2017-05-12 15:43:48 +0200

    Update pluginManager.py

--- server/pluginManager.py
+++ server/pluginManager.py
@@ -1,1 +1,26 @@
-LOL
+#!/usr/lib/python
+#Require python3.x
+#VOICEGRAM PluginManager.py v1.0 BETA
+#DEVELOPED BY ridolfino AND SourceCode2
+
+import socket
+
+class Voicegaming():
+  players=""
+  def conn(self,cmd):
+    HOST = ""
+    PORT = 5000
+    conn = socket.socket()
+    conn.connect((HOST,PORT))
+    if(cmd=="getPlayers"):
+      conn.send("getPlayers")
+      data=conn.recv(1024).decode()
+      players=data
+      conn.close()
+      return getPlayers()
+  def getPlayers(self):
+    conn("getPlayers")
+    return players
+    
+    
+
