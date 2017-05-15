8364b021c1223fba587ae5077a3dd4f159f28db7
Francesco <ridolfinotv@gmail.com>
2017-05-15 10:11:02 +0200

    Update getPlayers-Example.py

--- server/getPlayers-Example.py
+++ server/getPlayers-Example.py
@@ -1,18 +1,17 @@
 #import the framework
 import PluginManager
 
-#set server access point
-server.setServer("127.0.0.1",5000)
-
 #define the root
 server=PluginManager.Voicegram()
 
-#for open a request
-server.connect()
+#set server access point
+server.setServer("127.0.0.1",5000)
 
-#for split the players in array
+#for open a request
+server.getPlayers()
+
+#get players in the server 
 players=server.getPlayers()
-z=players.split("///")
 
 #print the result
 print(z)
