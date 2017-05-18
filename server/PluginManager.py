6150faec34d37a29b284694c518446bcd425dcfc
Francesco <ridolfinotv@gmail.com>
2017-05-18 11:20:25 +0200

    Update PluginManager.py
    I've add the password system

--- server/PluginManager.py
+++ server/PluginManager.py
@@ -9,41 +9,46 @@ class Voicegram():
   result=None
   HOST=""
   PORT=5000
+  PASSWD=""
   def conn(self,cmd):
     HOST = self.HOST
     PORT = self.PORT
+    PASSWD=self.PASSWD
 
     conn = socket.socket()
     conn.connect((HOST,PORT))
-
-    if(cmd=="getPlayers"):
-      data="getPlayers"
-      conn.send(data.encode())
-      data=conn.recv(1024).decode()
-      self.result=data
-      conn.close()
-      return data
-    if(cmd=="connect"):
-      data="getPlayers"
-      conn.send(data.encode())
-      data=conn.recv(1024).decode()
-      conn.close()
-      self.result=data
-      return data
-    if(cmd=="join"):
-      data="join"
-      conn.send(data.encode())
-      data=conn.recv(1024).decode()
-      self.result = data
-      conn.close()
-      return data
-    if(cmd=="msg"):
-      data="msg"
-      conn.send(data.encode())
-      data=conn.recv(1024).decode()
-      self.result = data
-      conn.close()
-      return data
+    data=PASSWD
+    conn.send(data.encode())
+    data=conn.recv(1024).decode()
+    if(data=="True"):
+        if(cmd=="getPlayers"):
+          data="getPlayers"
+          conn.send(data.encode())
+          data=conn.recv(1024).decode()
+          self.result=data
+          conn.close()
+          return data
+        if(cmd=="connect"):
+          data="getPlayers"
+          conn.send(data.encode())
+          data=conn.recv(1024).decode()
+          conn.close()
+          self.result=data
+          return data
+        if(cmd=="join"):
+          data="join"
+          conn.send(data.encode())
+          data=conn.recv(1024).decode()
+          self.result = data
+          conn.close()
+          return data
+        if(cmd=="msg"):
+          data="msg"
+          conn.send(data.encode())
+          data=conn.recv(1024).decode()
+          self.result = data
+          conn.close()
+          return data
     conn.close()
   def getPlayers(self):
       return self.conn("getPlayers")
@@ -54,6 +59,8 @@ class Voicegram():
         return self.conn("join")
     if(e=="onMsg"):
         return self.conn("msg")
-  def setServer(self,ip,port):
+  def setServer(self,ip,port,passwd):
     self.HOST=ip
     self.PORT=port
+    self.PASSWD=passwd
+   
