af5e466afa7b30d2504403784c6d5a8ee02458eb
Francesco <ridolfinotv@gmail.com>
2017-05-19 22:14:11 +0200

    Update getPlayers-Example.py

--- server/getPlayers-Example.py
+++ server/getPlayers-Example.py
@@ -4,8 +4,8 @@ import PluginManager
 #define the root
 server=PluginManager.Voicegram()
 
-#set connection to server(ip,port,passwd)
-server.setServer("127.0.0.1",8888,"abcd")
+#set connection to server(ip,port) the 8887 port is the PluginPort of VoiceGram
+server.setServer("127.0.0.1",8887)
 
 #get players in the server 
 players=server.getPlayers()
