c22df694433cc32f352e066574263fdf3e020de0
Francesco <ridolfinotv@gmail.com>
2017-05-25 14:41:09 +0200

    Update getPlayers-Example.py

--- Plugin/getPlayers-Example.py
+++ Plugin/getPlayers-Example.py
@@ -2,9 +2,9 @@
 import PluginManager
 
 #define the root
-server=PluginManager.Voicegram()
+server=PluginManager.Zitagram()
 
-#set connection to server(ip,port) the 8887 port is the PluginPort of VoiceGram
+#set connection to server(ip,port) the 8887 port is the PluginPort of Zitagram
 server.setServer("127.0.0.1",8887)
 
 #get players in the server 
