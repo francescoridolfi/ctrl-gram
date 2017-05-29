485da95e8cc32acf7b7352c810bb17bfac652ceb
SourceCode2 <davidesofronia@gmail.com>
2017-05-29 09:11:46 +0200

    Update client.c

--- client/client.c
+++ client/client.c
@@ -1,7 +1,7 @@
 /*
 FILE client.c
 Made by SourceCode2 and ridolfino
-Github repository : https://github.com/SourceCode2/Voicegram.git
+Github repository : https://github.com/SourceCode2/Zitagram.git
 LICENSE GPLv3
 */
 #include"../moduli/core.h"
