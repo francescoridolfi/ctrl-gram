9953ab3d18ca51e9d93ac395a7b6328c51a6080c
SourceCode2 <davidesofronia@gmail.com>
2017-06-21 15:31:16 +0200

    NULL

---
+++ plugin/Bot-Example.py
@@ -0,0 +1,13 @@
+import PluginManager
+
+bot=PluginManager.Bot()
+
+def function(conn):
+    data="Ciao!"
+    conn.send(data.encode())
+    data=conn.recv(1024).decode()
+    conn.close()
+    return data
+
+bot.setConn("127.0.0.1",8888,"Bot-Zitagram-Test")
+print(bot.newConn(function))
---
+++ plugin/PluginManager.py
@@ -0,0 +1,77 @@
+#!/usr/lib/python
+#Require python3.x
+#ZITAGRAM PluginManager.py v1.0 BETA
+#DEVELOPED BY ridolfino AND SourceCode2
+
+import socket
+from socketserver import ThreadingMixIn
+
+
+class Zitagram():
+  result=None
+  HOST="127.0.0.1"
+  PORT=8889
+  def conn(self,cmd):
+    HOST = self.HOST
+    PORT = self.PORT
+
+    conn = socket.socket()
+    conn.connect((HOST,PORT))
+    if(cmd=="getPlayers"):
+      data="getPlayers"
+      conn.send(data.encode())
+      data=conn.recv(1024).decode()
+      self.result=data
+      conn.close()
+      return data
+    if(cmd=="connect"):
+      data="getPlayers"
+      conn.send(data.encode())
+      data=conn.recv(1024).decode()
+      conn.close()
+      self.result=data
+      return data
+    if(cmd=="join"):
+      data="join"
+      conn.send(data.encode())
+      data=conn.recv(1024).decode()
+      self.result = data
+      conn.close()
+      return data
+    if(cmd=="msg"):
+      data="msg"
+      conn.send(data.encode())
+      data=conn.recv(1024).decode()
+      self.result = data
+      conn.close()
+      return data
+    conn.close()
+  def getPlayers(self):
+      return self.conn("getPlayers")
+  def connect(self):
+      return self.conn("connect")
+  def getEvent(self,e):
+    if(e=="onJoin"):
+        return self.conn("join")
+    if(e=="onMsg"):
+        return self.conn("msg")
+  def setServer(self,ip,port):
+    self.HOST=ip
+    self.PORT=port
+
+class Bot():
+  HOST="127.0.0.1"
+  PORT=8888
+
+  def newConn(self,function):
+    HOST = self.HOST
+    PORT = self.PORT
+
+    conn = socket.socket()
+    conn.connect((HOST,PORT))
+    return function(conn)
+
+  def setConn(self,ip,port,nick):
+    self.IP=ip
+    self.PORT=port
+    self.NICK=nick
