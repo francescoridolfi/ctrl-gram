ac8dcad0a0a5397d7af3818bb88f72ca458eaddb
SourceCode2 <davidesofronia@gmail.com>
2017-06-28 19:13:05 +0200

    GG

--- src/server.go
+++ src/server.go
@@ -1,31 +1,13 @@
 package main
 
-<<<<<<< HEAD
 import(
 	"net"
 	"fmt"
 	"bufio"
 	"strings"
 )
+
 func handler(conn net.Conn)  {
-=======
-import "net"
-import "fmt"
-import "bufio"
-import "strings" // only needed below for sample processing
-
-func main() {
-
-	fmt.Println("Launching server...")
-
-	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":8081")
-
-	// accept connection on port
-	conn, _ := ln.Accept()
-
-	// run loop forever (or until ctrl-c)
->>>>>>> 519a5b9045dd529e2e33187468bd3f012e6f85c7
 	for {
 		// will listen for message to process ending in newline (\n)
 		message, _ := bufio.NewReader(conn).ReadString('\n')
@@ -38,7 +20,6 @@ func main() {
 	}
 }
 func main() {
-
 	fmt.Println("Launching server...")
 
 	// listen on all interfaces
